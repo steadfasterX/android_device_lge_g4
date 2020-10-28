348b7bb32f5309adf27e81ef675f01f13a6ff1f2
stay steadfast <steadfasterX@users.noreply.github.com>
2020-10-28 17:45:23 +0100

    g4: board: fix torch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,9 +140,7 @@ SHRP_OTG := /usb_otg
 SHRP_FLASH := 1
 # These are led paths, find yours then put here (Optional)
 SHRP_CUSTOM_FLASHLIGHT := true
-SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
-SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
-SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+SHRP_FONP_1 := /sys/class/leds/torch-light1/brightness
 # Max Brightness of LED (Optional)
 SHRP_FLASH_MAX_BRIGHTNESS := 200
 # Check your device's recovery path, dont use blindly
